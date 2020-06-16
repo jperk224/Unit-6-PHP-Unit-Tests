ba289486ae3b610d079302384acd62f7c47f4f4a
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-16 09:04:35 -0400

    Add ListingBasic Exception Tests.

---
+++ tests/ListingBasicExceptionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * ListingBasic class tests
+ * @author Jeff Perkinson
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class ListingBasicExceptionTest extends TestCase
+{
+    /** @test */
+    function emptyConstructDataThrowsException() 
+    {
+        $this->expectExceptionMessage('Unable to create a listing, data unavailable');
+        $listingBasic = new ListingBasic();
+    }
+
+    /** @test */
+    function emptyIdThrowsException()
+    {
+        /**
+        * The class constructor calls setValues() using the data array passed into the
+        * constructor.  To bypass the first exception, data is needed, but no Id is
+        * provided in order to throw the 'invalid id' exception
+        */
+        $data = [
+            'title' => 'Test Title'
+        ];
+        $this->expectExceptionMessage('Unable to create a listing, invalid id');
+        $listingBasic = new ListingBasic($data);
+    }
+
+    /** @test */
+    function emptyTitleThrowsException()
+    {
+        /**
+        * The class constructor calls setValues() using the data array passed into the
+        * constructor.  To bypass the first exception, data is needed, but no title is
+        * provided in order to throw the 'invalid title' exception
+        */
+        $data = [
+            'id' => 1
+        ];
+        $this->expectExceptionMessage('Unable to create a listing, invalid title');
+        $listingBasic = new ListingBasic($data);
+    }
+}
